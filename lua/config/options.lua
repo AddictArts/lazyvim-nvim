bd32045dc2255735e1f7eaf0d48edcdfc9d34620
John Pywtorak <john.pywtorak@sri.com>
2025-12-15 21:10:28 -0800

    Fix terminal powershell pwsh.exe

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local this_os = vim.uv.os_uname().sysname
+
+if this_os:find("Windows_NT") then
+  -- Windows powershell use pwsh.exe
+  LazyVim.terminal.setup("pwsh.exe")
+end
